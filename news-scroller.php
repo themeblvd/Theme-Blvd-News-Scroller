c2cb00881c89dfb6700175cbb8e388c0897b1299
Jason Bobich <jasonbobich@gmail.com>
2012-12-19 13:36:01 -0900

    Starting v1.0.7 and reformatting to be like other ThemeBlvd plugins.

--- news-scroller.php
+++ news-scroller.php
@@ -2,29 +2,32 @@
 /*
 Plugin Name: Theme Blvd News Scroller Widget
 Description: This plugin is a simple widget with slider that rotates through posts.
-Version: 1.0.6
+Version: 1.0.7
 Author: Jason Bobich
 Author URI: http://jasonbobich.com
 License: GPL2
+
+    Copyright 2012  Jason Bobich
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2,
+    as published by the Free Software Foundation.
+
+    You may NOT assume that you can use any other version of the GPL.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    The license for this software can likely be found here:
+    http://www.gnu.org/licenses/gpl-2.0.html
+
 */
 
-/*
-Copyright 2012 JASON BOBICH
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License, version 2, as 
-published by the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
+define( 'TB_SCROLLER_PLUGIN_VERSION', '1.0.7' );
+define( 'TB_SCROLLER_PLUGIN_DIR', dirname( __FILE__ ) ); 
+define( 'TB_SCROLLER_PLUGIN_URI', plugins_url( '' , __FILE__ ) );
 
 /**
  * Include CSS
